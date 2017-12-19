7292245872b6cfc300abda9498f0b272552b93ed
Michelle Stuart <michelle.stuart@rutgers.edu>
2017-12-19 18:35:09 -0500

    commit

--- scripts/fish_rate.R
+++ scripts/fish_rate.R
@@ -1,6 +1,7 @@
 # a script to calculate the rate of fish processing by site
 
-# for 2017, every fish obs was recorded as an anem obs with the dive type C
+# collect all years with dive type C
+
 library(tidyverse)
 library(stringr)
 library(lubridate)
@@ -11,8 +12,7 @@ leyte <- read_db("Leyte")
 # pull a specific set of dives
 dive <- leyte %>% 
   tbl("diveinfo") %>% 
-  filter(dive_type == "C",
-    date > "2017-01-01") %>% 
+  filter(dive_type == "C") %>% 
   select(dive_table_id, date, site, gps) %>% 
   collect()
 
@@ -25,20 +25,44 @@ anem <- leyte %>%
   collect()
 
 anem <- left_join(anem, dive, by = "dive_table_id")
+anem <- anem %>% 
+  mutate(year = lubridate::year(date))
 
 # calculate the time between observations for each dive ----
-anem$obs_time <- lubridate::hms(anem$obs_time)
-anem$obs_time <- lubridate::as_date(anem$obs_time)
+anem <- anem %>% 
+  # mutate(obs_time <- lubridate::hms(obs_time)) %>% 
+  arrange(dive_table_id, obs_time)
 
-anem$pause <- NA
+anem$pause <- as.numeric(0.000)
 # calculate the time between observations
 for(i in 2:nrow(anem)){
-  anem$pause[i] <- lubridate::interval(anem$obs_time[i-1], anem$obs_time[i])
+  time1 <- hms(anem$obs_time[i])
+  time2 <- hms(anem$obs_time[i-1])
+  anem$pause[i] <- interval(time2, time1, origin = lubridate::origin)
+}
+
+anem <- anem %>% 
+  mutate(as.Date(obs_time)) %>% 
+  arrange(obs_time) %>% 
+  select(-obs_time)
+
+# remove the first row of each dive: the time since last dive
+dive_ids <- distinct(anem, dive_table_id)
+for(i in 1:nrow(dive_ids)){
+  # pull out only one dive_table_id at a time
+  x <- anem %>% 
+    filter(dive_table_id == dive_ids$dive_table_id[i]) 
+  # remove those dives from the main table
+  anem <- anti_join(anem, x, by = "dive_table_id")
+  # remove the first row from x (should be the gap between dives)
+  x <- x[2:nrow(x), ] 
+  # rejoin to the main group
+  anem <- rbind(anem, x)
 }
 
 anem <- anem %>% 
   mutate(pause = pause/60) %>% 
-  select(dive_table_id, pause, site)
+  select(dive_table_id, pause, site, year)
 
 anem <- anem %>%  # convert to minutes
   filter(pause > 0, 
