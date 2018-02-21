cc826daa8651de8092cbe1adfec9ec0822e09b22
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-02-21 13:12:09 -0500

    update code for 2018 - remove excel reading, going back to csv - remove type-o checks that will be caught with data validation on spreadsheet

--- scripts/01checkxl.R
+++ scripts/01checkxl.R
@@ -1,103 +1,114 @@
 # ---------------------------------------------
-#   Set up work space - load packages and data
+#   Set up work space - load packages and data ####
 # ---------------------------------------------
 library(tidyverse)
 library(stringr)
 source("scripts/field_helpers.R")
-excel_file <- ("data/GPSSurveys2017.xlsx")
-pitfile <- ("data/BioTerm.txt" )
+
+# if data is accessible in google sheets:
+library(googlesheets)
+# gs_auth(new_user = TRUE) # run this if having authorization problems
+mykey <- '1symhfmpQYH8k9dAvp8yV_j_wCTpIT8gO9No4s2OIQXo' # access the file
+entry <-gs_key(mykey)
+clown <-gs_read(entry, ws='clownfish')
+dive <- gs_read(entry, ws="diveinfo")
+
+
+# # if data is via csv
+# dat <- ("~/Downloads/2018_clownfish_data_entry - clownfish.csv")
+
+# include pit tag scanner output
+pitfile <- ("data/BioTerm.txt")
+# pitfile <- ("~/Downloads/BioTerm.txt" )
+
 problem <- data.frame()
 
-anemcol <- c("text", "text", "text", "text", "date", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text")
-
-clowncol <- c("text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text", "text")
 
 # ---------------------------------------------
-#   adjust formatting
+#   adjust formatting ####
 # ---------------------------------------------
-dive <- excl("diveinfo", NULL)
 names(dive) <- stringr::str_to_lower(names(dive))
-dive <- filter(dive, !is.na(divenum))
-anem <- excl("anemones", anemcol)
-names(anem) <- stringr::str_to_lower(names(anem))
-anem <- filter(anem, !is.na(divenum))
-clown <- excl("clownfish", clowncol)
+dive <- filter(dive, !is.na(dive_num))
+# no separate anemone sheet in 2018
+# anem <- excl("anemones", anemcol)
+# names(anem) <- stringr::str_to_lower(names(anem))
+# anem <- filter(anem, !is.na(divenum))
 names(clown) <- stringr::str_to_lower(names(clown))
-clown <- filter(clown, !is.na(divenum))
+clown <- filter(clown, !is.na(dive_num))
+
+# # ---------------------------------------------
+# #   check the diveinfo sheet for type-o's #### don't need to do this because using data validation in the spreadsheet
+# # ---------------------------------------------
+# sites <- c("Palanas", "Wangag", "Magbangon", "Cabatoan", "Caridad Cemetery", "Caridad Proper", "Hicgop", "Hicgop South", "Sitio Tugas", "Elementary School", "Sitio Lonas", "San Agustin", "Poroc San Flower", "Poroc Rose", "Visca", "Gabas", "Tamakin Dacot", "Haina", "Sitio Baybayon")
+# good <- filter(dive, site %in% sites)
+# bad <- anti_join(dive, good)
+# bad <- filter(bad, !is.na(divenum))
+# if (nrow(bad) > 0){
+#   bad$typeo <- "fix site spelling on diveinfo table"
+# }
+# (problem <- rbind(problem, bad))
+# rm(good, bad)
 
 # ---------------------------------------------
-#   check the diveinfo sheet for type-o's
-# ---------------------------------------------
-sites <- c("Palanas", "Wangag", "Magbangon", "Cabatoan", "Caridad Cemetery", "Caridad Proper", "Hicgop", "Hicgop South", "Sitio Tugas", "Elementary School", "Sitio Lonas", "San Agustin", "Poroc San Flower", "Poroc Rose", "Visca", "Gabas", "Tamakin Dacot", "Haina", "Sitio Baybayon")
-good <- filter(dive, site %in% sites)
-bad <- anti_join(dive, good)
-bad <- filter(bad, !is.na(divenum))
-if (nrow(bad) > 0){
-  bad$typeo <- "fix site spelling on diveinfo table"
-}
-(problem <- rbind(problem, bad))
-rm(good, bad)
-
-# ---------------------------------------------
-#   check the anemones sheet for type-o's
+#   check the anemones sheet for type-o's #### don't need to do this because using data validation in the spreadsheet
 # ---------------------------------------------
 
-# check anem species
-anems <- c("ENQD", "STME", "HECR", "HEMG", "STHD", "HEAR", "MADO", "HEMA", "STGI", "????", "EMPT")
-good <- filter(anem, anemspp %in% anems)
-bad <- anti_join(anem, good)
-bad <- filter(bad, !is.na(anemspp))
-if (nrow(bad) > 0){
-  bad$typeo <- "fix anem spp on anem table"
-}
-(problem <- rbind(problem, bad))
-rm(good, bad)
-
-# check fish species
-fish <- c("APCL", "APOC", "APPE", "APSE", "APFR", "APPO", "APTH", "PRBI", "NA")
-good <- filter(anem, spp %in% fish)
-bad <- anti_join(anem, good) # wait for the next line before checking bad
-bad <- filter(bad, !is.na(spp))
-if (nrow(bad) > 0){
-  bad$typeo <- "fix fish spp on anem table"
-}
-(problem <- rbind(problem, bad))
-rm(good, bad)
+# # check anem species
+# anems <- c("ENQD", "STME", "HECR", "HEMG", "STHD", "HEAR", "MADO", "HEMA", "STGI", "????", "EMPT")
+# good <- filter(anem, anemspp %in% anems)
+# bad <- anti_join(anem, good)
+# bad <- filter(bad, !is.na(anemspp))
+# if (nrow(bad) > 0){
+#   bad$typeo <- "fix anem spp on anem table"
+# }
+# (problem <- rbind(problem, bad))
+# rm(good, bad)
+# 
+# # check fish species
+# fish <- c("APCL", "APOC", "APPE", "APSE", "APFR", "APPO", "APTH", "PRBI", "NA")
+# good <- filter(anem, spp %in% fish)
+# bad <- anti_join(anem, good) # wait for the next line before checking bad
+# bad <- filter(bad, !is.na(spp))
+# if (nrow(bad) > 0){
+#   bad$typeo <- "fix fish spp on anem table"
+# }
+# (problem <- rbind(problem, bad))
+# rm(good, bad)
 
 # ---------------------------------------------
 #   check the clownfish sheet for type-o's
 # ---------------------------------------------
 
-# Check fish species
-good <- filter(clown, spp %in% fish)
-bad <- anti_join(clown, good)
-bad <- filter(bad, !is.na(spp))
-if (nrow(bad) > 0){
-  bad$typeo <- "fix fish spp on fish table"
-}
-(problem <- rbind(problem, bad))
+# # Check fish species #### don't need to do this because using data validation in the spreadsheet
+# good <- filter(clown, spp %in% fish)
+# bad <- anti_join(clown, good)
+# bad <- filter(bad, !is.na(spp))
+# if (nrow(bad) > 0){
+#   bad$typeo <- "fix fish spp on fish table"
+# }
+# (problem <- rbind(problem, bad))
+# 
+# # check tail colors
+# colors <- c("YE", "O", "YR", "YP", "Y", "W", "WR", "WP", "BW", "B")
+# good <- filter(clown, color %in% colors)
+# bad <- anti_join(clown, good)
+# bad <- filter(bad, !is.na(color))
+# if (nrow(bad) > 0){
+#   bad$typeo <- "fix tail color on fish table"
+# }
+# (problem <- rbind(problem, bad))
 
-# check tail colors
-colors <- c("YE", "O", "YR", "YP", "Y", "W", "WR", "WP", "BW", "B")
-good <- filter(clown, color %in% colors)
-bad <- anti_join(clown, good)
-bad <- filter(bad, !is.na(color))
-if (nrow(bad) > 0){
-  bad$typeo <- "fix tail color on fish table"
-}
-(problem <- rbind(problem, bad))
-
-# Are there anems are on the clownfish sheet that are not on the anemone sheet?
-clowndive <- clown$divenum
-clowndive <- unique(clowndive)
-
-bad <- compare_dives(clowndive)
-
-if (nrow(bad) > 0){
-  bad$typeo <- "anemid in fish table doesn't match anem data table"
-}
-problem <- rbind(problem, bad)
-rm(bad, good)
+# # Are there anems are on the clownfish sheet that are not on the anemone sheet? # don't need to do this because not an anem sheet in 2018
+# clowndive <- clown$divenum
+# clowndive <- unique(clowndive)
+# 
+# bad <- compare_dives(clowndive)
+# 
+# if (nrow(bad) > 0){
+#   bad$typeo <- "anemid in fish table doesn't match anem data table"
+# }
+# problem <- rbind(problem, bad)
+# rm(bad, good)
 
 # Are there repeat ID numbers on the clownfish sheet?
 dups <- clown %>% 
