1f1782eaeadaafbfaa917a1dad423c75e5ec2101
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-03-07 18:30:01 -0500

    updating for 2018 field season

--- scripts/04_divesummary.R
+++ scripts/04_divesummary.R
@@ -1,13 +1,36 @@
 # This is a script to determine for each dive the number of fish captured, sampled and recatpured and the same numbers at each site, also the number of dives per site.
+
+# ---------------------------------------------
+#   Set up work space - load packages and data
+# ---------------------------------------------
 library(tidyverse)
 library(stringr)
+source("scripts/field_helpers.R")
 
-excel_file <- ("data/GPSSurveys2017.xlsx")
+# if data is accessible in google sheets: 
+#ALLISON NOTE: uncommented this section
+library(googlesheets)
+# gs_auth(new_user = TRUE) # run this if having authorization problems
+mykey <- '1symhfmpQYH8k9dAvp8yV_j_wCTpIT8gO9No4s2OIQXo' # access the file
+entry <-gs_key(mykey)
+clown <-gs_read(entry, ws='clownfish')
+dive <- gs_read(entry, ws="diveinfo")
+
+# save data in case network connection is lost
+clownfilename <- str_c("data/clown_", Sys.time(), ".Rdata", sep = "")
+divefilename <- str_c("data/dive_", Sys.time(), ".Rdata", sep = "")
+save(clown, file = clownfilename)
+save(dive, file = divefilename)
+
+# # load data from saved if network connection is lost
+# # THIS HAS TO BE MANUALLY UPDATED WITH MOST CURRENT VERSION OF SAVED FILE  - COULD WRITE CODE TO FIND AND LOAD THE MOST CURRENT VERSION ####
+# load(file = "data/clown_2018-03-07 17:25:56.Rdata")
+# load(file = "data/dive_2018-03-07 17:25:56.Rdata")
 
 # fish observation info from anem survey
-fish <- readxl::read_excel(excel_file, sheet = "anemones", col_names = TRUE, na = "")
+fish <- clown
 names(fish) <- str_to_lower(names(fish))
-fish <- filter(fish, !is.na(id)) %>% 
+fish <- filter(fish, !is.na(fish_spp)) %>% 
   distinct()
 
 # get dive info
