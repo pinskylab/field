ce34f864c630617a7d6ff2df5c4d06cced9add39
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-03-17 05:16:04 +0800

    add code for tags that were captured more than once in a season

--- scripts/04_divesummary.R
+++ scripts/04_divesummary.R
@@ -47,7 +47,7 @@ samp <- clown
 names(samp) <- stringr::str_to_lower(names(samp))
 samp <- filter(samp, !is.na(dive_num))
 
-# make a table of observed fish
+# make a table of observed APCL
 divetot <- fish %>%
   group_by(dive_num) %>% 
   filter(fish_spp == "APCL") %>% 
@@ -63,19 +63,14 @@ tissue <- samp %>%
   summarize(fins=n())
 divetot <- left_join(divetot, tissue, by = "dive_num")
 
-# how many fish are captured (clipped + recaptures) - this will miss fish that were captured by escaped before getting a clip...
+# how many fish are captured (clipped + recaptures) - this will miss fish that were captured but escaped before getting a clip...
 captured <- samp %>% 
-  filter(recap == "Y" | !is.na(fin_id)) %>% #filter out recaps or fish with fin-clips (looks like it handles recap fish with a clip correctly by only counting them once)
+  filter(recap == "Y" | !is.na(fin_id)) %>% # select fish that either already had a tag or were given a fin clip (only counts fish that were both Y and clipped once)
   group_by(dive_num) %>%
   summarise(captured = n())
 
 divetot <- left_join(divetot, captured, by = "dive_num")
 
-# # how many fish were captured - doesn't work for 2018 because we guessed sizes of uncaptured fish
-# cap <- samp %>% filter(!is.na(size)) %>% 
-#     group_by(dive_num) %>% 
-#     summarise(captured = n())
-# divetot <- left_join(divetot, cap, by = "dive_num")
 
 # how many fish were recaptures?
 recap <- samp %>% 
@@ -90,8 +85,9 @@ clipped_recaps <- samp %>%
   filter(recap == "Y" & !is.na(fin_id)) %>%
   group_by(dive_num) %>%
   summarise(clipped_recaps = n())
-
 divetot <- left_join(divetot, clipped_recaps, by = "dive_num")
+
+
 
 divetot <- divetot %>% 
   select(dive_num, site, observed, captured, fins, recap, clipped_recaps) %>% 
@@ -148,11 +144,47 @@ clipped_Rs <- fishobs %>%
 
 sitetot <- left_join(sitetot, clipped_Rs, by = "site")
 
+# which tags have been entered into the data more than once
+rep_tag <- fish %>% 
+  filter(!is.na(tag_id)) %>% # get only obs with tags
+  mutate(tag_id = str_sub(tag_id, -6)) %>% # make sure all tag_ids are only 6 digits
+  group_by(tag_id) %>% # pull same tags together
+  summarise(count = n()) %>% # count the same tags
+  filter(count > 1) # choose only those that were found more than once
+# how many captures were repeats of a tag already caught this season
+same_seas_recap <- fish %>% 
+  mutate(tag_id = str_sub(tag_id, -6)) %>% # make sure all tag_ids are only 6 digits
+  filter(tag_id %in% rep_tag) # can't do recap == Y because the first capture of this year might also have been a recap
+# add counts of scans
+same_seas_recap <- left_join(same_seas_recap, rep_tag, by = "tag_id")
+rm(rep_tag)
+# add dive info
+same_seas_recap <- left_join(same_seas_recap, dive, by = "dive_num")
+# reduce columns & remove repeated info
+same_seas_recap <- same_seas_recap %>% 
+  select(tag_id, count, site) %>% 
+  distinct()
+# subtract 1 from count for the initial capture this year
+same_seas_recap <- same_seas_recap %>% 
+  mutate(count = count - 1) %>% 
+  select(-tag_id) %>% 
+  rename(same_season_recap = count)
+
+sitetot <- left_join(sitetot, same_seas_recap, by = "site")
+
+# how many dives have been done at each site?
+dive_count <- dive %>% 
+  select(dive_num, site) %>% 
+  distinct() %>%  # remove muliple dive records for the same dive due to multipe gps units
+  group_by(site) %>% 
+  summarise(number_dives = n())
+sitetot <- left_join(sitetot, dive_count, by = "site")
+
 sitetot <- sitetot %>% 
-  select(site, observed, captured, finclip, recap, clipped_recaps)
+  select(site, number_dives, observed, captured, finclip, recap, clipped_recaps, same_season_recap)
 
 # are any captured more than observed?
-bad <- filter(sitetot, captured > observed) # the one at visca is ok because it is ????
+bad <- filter(sitetot, captured > observed) # 2017: the one at visca is ok because it is "????"
 # are there more fin clips than captured?
 bad <- filter(sitetot, finclip > captured)
 # are there ore recaps than captured?
@@ -161,9 +193,13 @@ bad <- filter(sitetot, recap > captured)
 # do the divetot numbers match up with the sitetot? 
 divetotsum <- divetot %>% #calculate site totals from divetot, can compare to sitetot
   group_by(site) %>% 
-  summarise(site_observed = sum(observed), site_captured = sum(captured, na.rm = TRUE), site_fins = sum(fins, na.rm = TRUE), site_recap = sum(recap, na.rm = TRUE), site_clipped_recaps = sum(clipped_recaps, na.rm = TRUE))
+  summarise(site_observed = sum(observed), 
+    site_captured = sum(captured, na.rm = TRUE), 
+    site_fins = sum(fins, na.rm = TRUE), 
+    site_recap = sum(recap, na.rm = TRUE), 
+    site_clipped_recaps = sum(clipped_recaps, na.rm = TRUE))
 
-# #are the observed fish per site the same? (will print message if so, if no message all is good) #doesn't quite work b/c doesn't compare NAs (in sitetot) to 0s (in divetotsum) correctly
+# #are the observed fish per site the same fish recaptured more than once in a season? (will print message if so, if no message all is good) #doesn't quite work b/c doesn't compare NAs (in sitetot) to 0s (in divetotsum) correctly
 # for (i in 1:length(divetotsum$site)) {
 #   site_name <- divetotsum$site[i]
 #   if (divetotsum$site_observed[i] != sitetot$observed[i]) {
