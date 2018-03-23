0579fa94da6810c4cc6adf93d902f6c24187a5b4
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-03-24 06:46:46 +0800

    convert gpx assignment into a function

--- scripts/03anemstoqgis.R
+++ scripts/03anemstoqgis.R
@@ -13,110 +13,48 @@ source("scripts/readGPXGarmin.R")
 
 get_from_google()
 
-
-# # load data from saved if network connection is lost
-# # THIS HAS TO BE MANUALLY UPDATED WITH MOST CURRENT VERSION OF SAVED FILE  - COULD WRITE CODE TO FIND AND LOAD THE MOST CURRENT VERSION ####
-# load(file = "data/clown_2018-03-12 21:13:51.Rdata")
-# load(file = "data/dive_2018-03-12 21:13:52.Rdata")
-
+# # load data from saved if network connection is lost ####
+# # get list of files 
+# clown_files <- sort(list.files(path = "data/", pattern = "clown_201*"), decreasing = T)
+# dive_files <- sort(list.files(path = "data/", pattern = "dive_201*"), decreasing = T)
+# load(file = paste("data/", clown_files[1], sep = ""))
+# load(file = paste("data/", dive_files[1], sep = ""))
 
 anem <- clown %>% 
-  filter(!is.na(dive_num)) %>%
-  distinct() %>% 
-  mutate(gps = as.integer(gps))
-
-
-anem$id <- 1:nrow(anem) # add row numbers so that gpx can be added later (creates 4 rows for each anem)
-
-names(dive) <- tolower(names(dive))
-dive <- dive %>%
-  filter(dive_num %in% anem$dive_num) %>%
-  select(dive_num, date, site, municipality, cover, gps) %>% 
-  distinct()
-
+  filter(!is.na(anem_spp)) %>% # we only want anem_obs, not fish_obs 
+  select(dive_num, obs_time, gps, anem_id)
 # if gps was not specified on the line, use Michelle's gps because she was the anem obs person on the early dives.
 anem <- anem %>% 
-  mutate(gps = ifelse(is.na(gps), 4, gps))
+  mutate(gps = ifelse(is.na(gps), 4, gps)) %>% 
+  filter(!is.na(anem_id)) # now that we have only anem_obs, want only anems with ids
 
+dive <- dive %>%
+  filter(dive_num %in% anem$dive_num) %>%
+  select(dive_num, date, gps) %>% 
+  distinct()
 
 # ---------------------------------------------
 #   add dates and sites to anems
 # ---------------------------------------------
 
-anem <- left_join(anem, dive, by = c("dive_num", "gps"))
+anem <- left_join(anem, dive)
 
 # create a table of the lines that do not have a time and remove them
 no_gps <- anem %>% 
   filter(is.na(obs_time))
 anem <- anti_join(anem, no_gps)
 
-# remove lines that are not anemones and remove weird dates that excel attaches and convert time zone
+# combine the date_time and assign the Philippines time zone
 anem <- anem %>%
-  filter(!is.na(anem_spp)) %>% 
   mutate(obs_time = force_tz(ymd_hms(str_c(date, obs_time, sep = " ")), tzone = "Asia/Manila"), 
     gps = as.character(gps))
 
 # convert to UTC
 anem <- mutate(anem, obs_time = with_tz(obs_time, tzone = "UTC"))
 
-# split out time components to compare to latlong
-anem <- anem %>%
-  mutate(month = month(obs_time)) %>%
-  mutate(day = day(obs_time)) %>%
-  mutate(hour = hour(obs_time)) %>%
-  mutate(min = minute(obs_time)) %>%
-  mutate(sec = second(obs_time)) %>%
-  mutate(year = year(obs_time))
+# in order to use the **assign_gpx function**, need a table that contains an id, gps unit, and a date_time that have been converted to UTC time zone - the date_time column must be called "obs_time"
 
-# create table of lat lon data
-# make an empty data frame for later
-gpx <- data.frame()
-
-# define the list of gps units
-gps <- name_gps()
-
-# determine which gps units have data & remove empties
-gps <- used_gps(gps)
-
-for (l in 1:length(gps)){
-  files <- list.files(path = paste("data/",gps[l], sep = ""), pattern = "*Track*")
-  for(i in 1:length(files)){ # for each file
-    dat <- prep_gpx(gps, files) # parse the gpx into useable data
-    gpx <- rbind(gpx, dat)
-  }
-}
-
-gpx <- gpx %>%
-  mutate(month = month(time)) %>%
-  mutate(day = day(time)) %>%
-  mutate(hour = hour(time)) %>%
-  mutate(min = minute(time)) %>%
-  mutate(sec = second(time))
-
-# fix formatting
-gpx$lat <- as.character(gpx$lat) # otherwise they import as factors
-gpx$lon <- as.character(gpx$lon)
-gpx$time <- as.character(gpx$time)
-
-# find matches for times to assign lat long - there are more than one set of seconds (sec.y) that match
-anem <- left_join(anem, gpx, by = c("month", "day", "hour", "min", c("gps" = "unit")))
-anem$lat <- as.numeric(anem$lat)
-anem$lon <- as.numeric(anem$lon) # need to make decimal 5 digits - why? because that is all the gps can hold
-
-coord <- anem %>%
-  group_by(id) %>%
-  summarise(mlat = mean(lat, na.rm = TRUE),
-    mlon = mean(lon, na.rm = T))
-
-
-# drop all of the unneccessary columns from anem and join with the coord
-anem <- select(anem, id, anem_spp, anem_id, fish_spp, obs_time, site)
-
-anem <- left_join(coord, anem, by = "id")
-anem <- rename(anem, lat = mlat, lon = mlon)
-
-anem <- select(anem, -id)
-anem <- distinct(anem)
+coord_table <- assign_gpx_field(anem)
 
 
 # Sort the data
