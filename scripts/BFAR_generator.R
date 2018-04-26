0d91554f472742abf3d1b6adbf62f6cdd0b7339a
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-04-26 18:02:37 -0400

    trying to run report - got errors

--- scripts/BFAR_generator.R
+++ scripts/BFAR_generator.R
@@ -1,3 +1,7 @@
+source("scripts/field_helpers.R")
+library(knitr)
+library(rmarkdown)
+
 #' ---
 #' title: Annual Report for Gratuitous Permit No. FBP-0079-14
 #' author: Malin Pinsky
@@ -10,9 +14,9 @@
 #' ### Introduction
 #' Under the project “Effects of low population density on reef fish
 #' connectivity,” we conducted fieldwork and collected anemonefish samples in
-#' Leyte from May 1 to June 16, 2016. The primary objective was to collect
+#' Leyte from `r min(bfar$date)` to `r max(bfar$date)`. The primary objective was to collect
 #' tissue samples (fin clips) from individuals of Amphiprion clarkii at each of
-#' `r nrow(bfar)` locations. In this season, we sampled `r nrow(bfar)` sites in Albuera and Bay Bay City, Leyte.
+#' `r nrow(bfar)` locations. In this season, we sampled `r nrow(bfar)` sites in Albuera and Baybay City, Leyte.
 #'
 #' We are now conducting laboratory analysis of the collected specimens. We are
 #' using genotyping-by-sequencing methods to genotype each specimen at a number
@@ -26,40 +30,53 @@
 #' Note: All samples are tissue clips from the caudal fin.
 #' <!-- generate a table for the bfar report - in the field, load 2017 dive data from saved database -->
 ```{r echo = F}
-dive <- read.csv("/Users/macair/Philippines_Docs/field/data/diveinfo.csv", stringsAsFactors = F) %>% 
-  filter(grepl("2017", date)) %>% 
-  select(dive_table_id, site, municipality)
-# in the field, load the anemones that correspond to those dives
-anem <- read.csv("/Users/macair/Philippines_Docs/field/data/anemones.csv", stringsAsFactors = F) %>% 
+leyte <- read_db("Leyte")
+dive <- leyte %>% 
+  tbl("diveinfo") %>%
+  collect() %>%  
+  filter(grepl("2017", date))  %>% 
+  select(dive_table_id, site, municipality, date) 
+
+anem <- leyte %>% 
+  tbl("anemones") %>% 
+  collect() %>% 
   filter(dive_table_id %in% dive$dive_table_id) %>% 
   select(anem_table_id, dive_table_id) %>% 
   mutate(dive_table_id = as.integer(dive_table_id), 
     anem_table_id = as.integer(anem_table_id))
+
 anem <- left_join(anem, dive, by = "dive_table_id")
 rm(dive)
-# in the field, load the fish that correspond to those anemones
-fish <- read.csv("/Users/macair/Philippines_Docs/field/data/clownfish.csv", stringsAsFactors = F) %>% 
+
+fish <- leyte %>% 
+  tbl("clownfish") %>% 
+  collect() %>% 
   filter(anem_table_id %in% anem$anem_table_id, 
     !is.na(fin_id), fin_id != "NULL") %>% 
   select(fish_table_id, anem_table_id, fin_id)
+
 fish <- left_join(fish, anem, by = "anem_table_id")
 rm(anem)
+
 bfar <- fish %>% 
-  group_by(site, municipality) %>%
+  group_by(site, municipality, date) %>%
   summarise(samples = n()) %>% 
   mutate(Province = "Leyte") %>% 
-  select(Province, municipality, site, samples)
+  select(Province, municipality, site, samples, date)
+
+# format for report
 names(bfar) <- stringr::str_to_title(names(bfar))
 bfar$Site_number <- 1:nrow(bfar)
-bfar <- bfar[, c(5, 1, 2, 3, 4)]
 
-knitr::kable(bfar)
+# reorder columns
+bfar <- bfar[, c("Site_number", "Province", "Municipality", "Site", "Samples")]
+
+kable(bfar)
 
 map_path <- "/Users/macair/Philippines_Docs/field/data/leyte_map.png"
 include_graphics(map_path)
+```
 
 
 #' <!--highlight the line below within the comments starting at rmarkdown and ending at the ) and command-enter to generate the report -->
-#' 
-#' <!-- rmarkdown::render('/Users/macair/Philippines_Docs/field/scripts/BFAR_generator.R') -->
-
+rmarkdown::render('/Users/Whitmore/Desktop/BFAR_generator.R')
