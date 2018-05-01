256960372609daa8a88eca4b987a60537ad3afad
Michelle Stuart <michelle.stuart@rutgers.edu>
2018-05-01 09:40:16 -0400

    moving repos

--- scripts/ideas for code.R
+++
@@ -1,3 +0,0 @@
-# write a code that finds every -9999 and creates a text file (individually) to import into QGIS to find the actual anem_id
-
-# create a column called doubt with a 0 1 - if it has a 1 for doubt, look at the notes and see what the cause of the doubt is
